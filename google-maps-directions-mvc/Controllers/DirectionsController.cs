eee1fc9a24ebac988d9b97deee4364457e133943
= <=>
2018-11-22 12:32:35 +0530

    First commit

---
+++ google-maps-directions-mvc/Controllers/DirectionsController.cs
@@ -0,0 +1,35 @@
+﻿using google_maps_directions_dotnet.Models;
+using System;
+using System.Collections.Generic;
+using System.Collections.Specialized;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Threading.Tasks;
+using System.Web.Http;
+
+namespace google_maps_directions_dotnet.Controllers
+{
+    public class DirectionsController : ApiController
+    {
+        DirectionsProvider m_directionsProvider = new DirectionsProvider();
+
+        // GET: api/Directions
+        /// <summary>
+        /// Get directions from Google Directions API
+        /// </summary>
+        /// <returns>Directions</returns>
+        public async Task<Directions> Get(string from, string to)
+        {
+            if (string.IsNullOrWhiteSpace(from) || string.IsNullOrWhiteSpace(to))
+            {
+                throw new HttpResponseException(HttpStatusCode.BadRequest);
+            }
+
+            // We seem to have params. Get the directions.
+            Directions dirs = await m_directionsProvider.getDirections(from, to);
+
+            return dirs;
+        }
+    }
+}
